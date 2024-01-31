14d22d69d483be048ec766e8450bb9fd651e4f58
Any-Winter-4079 <50542132+Any-Winter-4079@users.noreply.github.com>
2024-01-31 21:49:33 +0100

    Handle camera reinitialisation after updating the config

--- computer_code/test_scripts/camera/1_update_cam_config.py
+++ computer_code/test_scripts/camera/1_update_cam_config.py
@@ -22,13 +22,52 @@ import urllib.request
 # In my case: source ./tensorflow-metal-test/bin/activate (from v2 folder)
 
 #################
+# Notes         #
+#################
+# Lowering the clock frequency (from 20MHz to 8MHz max) makes the image clearer,
+# which may improve processes such as camera calibration and image stitching.
+
+# Make sure you don't see:
+# Error 501: Not Implemented
+# or that'll mean the camera config update didn't work (even if OpenCV still displays an image
+# - it may not be of the desired size or quality).
+# For example, I tend to see it for too low JPEG_QUALITY values (very high quality).
+
+# If you lose the image stream, the camera may reset and hence lose the updated configuration
+# (i.e. the camera will reinitialize with the default FRAME_SIZE and JPEG_QUALITY in the code).
+# Because of that, when the stream is recovered, we have to update the configuration again.
+# Some waiting time is advisable to give the camera breathing room to recover and reset.
+
+# M5Stack Wide Camera
+
+# | FRAME_SIZE      | JPEG_QUALITY 4 | JPEG_QUALITY 8 | JPEG_QUALITY 16 | JPEG_QUALITY 32 | JPEG_QUALITY 63 |
+# |-----------------|----------------|----------------|-----------------|-----------------|-----------------|
+# | QVGA (320x240)  | Err            | 0.045          | 0.035           | 0.032           | 0.030           |
+# | VGA (640x480)   | 0.141          | 0.112          | 0.077           | 0.074           | 0.067           |
+# | SVGA (800x600)  | 0.177          | 0.126          | 0.113           | 0.075           | 0.067           |
+# | XGA (1024x768)  | 0.365          | 0.282          | 0.197           | 0.153           | 0.152           |
+# | SXGA (1280x1024)| 0.491          | 0.335          | 0.248           | 0.183           | 0.166           |
+# | UXGA (1600x1200)| 0.618          | 0.409          | 0.300           | 0.222           | 0.191           |
+
+# Ai-Thinker Camera
+
+# | FRAME_SIZE      | JPEG_QUALITY 4 | JPEG_QUALITY 8 | JPEG_QUALITY 16 | JPEG_QUALITY 32 | JPEG_QUALITY 63 |
+# |-----------------|----------------|----------------|-----------------|-----------------|-----------------|
+# | QVGA (320x240)  | Err            | 0.039          | 0.029           | 0.030           | 0.027           |
+# | VGA (640x480)   | 0.139          | 0.118          | 0.074           | 0.075           | 0.068           |
+# | SVGA (800x600)  | 0.143          | 0.111          | 0.107           | 0.067           | 0.067           |
+# | XGA (1024x768)  | 0.286          | 0.269          | 0.182           | 0.148           | 0.150           |
+# | SXGA (1280x1024)| 0.374          | 0.258          | 0.212           | 0.173           | 0.155           |
+# | UXGA (1600x1200)| 0.503          | 0.307          | 0.218           | 0.221           | 0.194           |
+
+#################
 # Configuration #
 #################
-JPEG_QUALITY = 12 # 0-63 lower means higher quality
-FRAME_SIZE = "FRAMESIZE_VGA" # FRAMESIZE_QVGA: 320x240, FRAMESIZE_VGA: 640x480, FRAMESIZE_SVGA: 800x600, FRAMESIZE_XGA: 1024x768, FRAMESIZE_SXGA: 1280x1024, FRAMESIZE_UXGA: 1600x1200
+JPEG_QUALITY = 32 # 0-63 lower means higher quality
+FRAME_SIZE = "FRAMESIZE_UXGA" # FRAMESIZE_QVGA: 320x240, FRAMESIZE_VGA: 640x480, FRAMESIZE_SVGA: 800x600, FRAMESIZE_XGA: 1024x768, FRAMESIZE_SXGA: 1280x1024, FRAMESIZE_UXGA: 1600x1200
 USE_HOTSPOT = False
 M5STACKWIDE_CAMERA = False
-ITERATIONS = 200
+NUM_ITERATIONS = 1000
 
 ip = "*.*.*.*" if M5STACKWIDE_CAMERA and USE_HOTSPOT else "*.*.*.*" if M5STACKWIDE_CAMERA else "*.*.*.*" if USE_HOTSPOT else "*.*.*.*"  #  ESP32-CAM IPs, e.g. (192, 168, 1, 181).            ** Replace **
 esp32_config_url = f"http://{ip}/camera_config"
@@ -59,9 +98,17 @@ def update_camera_config(jpeg_quality, frame_size):
 
 def main():
     total_time = 0
+    stream_to_recover = False
+    stream_active = False
     update_camera_config(JPEG_QUALITY, FRAME_SIZE)
 
-    for _ in range(ITERATIONS):
+    for _ in range(NUM_ITERATIONS):
+
+        if stream_to_recover and stream_active:
+            print("Stream recovered.")
+            update_camera_config(JPEG_QUALITY, FRAME_SIZE)
+            stream_to_recover = False
+            stream_active = False
         
         fetch_start = time.time()
         img = fetch_image_with_timeout(esp32_image_url)
@@ -69,14 +116,19 @@ def main():
 
         if img is None:
             print(f"No image from {esp32_image_url}.")
+            stream_to_recover = True
+            # Wait for the camera to recover
+            cv2.waitKey(1000)
             continue
+        else:
+            stream_active = True
         cv2.imshow("Image", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         total_time += (fetch_end - fetch_start)
 
-    average_time = total_time / ITERATIONS
-    print(f"\nAverage fetch time over {ITERATIONS} iterations: {average_time:.2f} seconds")
+    average_time = total_time / NUM_ITERATIONS
+    print(f"\nAverage fetch time over {NUM_ITERATIONS} iterations: {average_time:.3f} seconds")
 
 if __name__ == "__main__":
     main()
