1fa2e39b1371fd19b0ea9197619b70c6a6915ac6
Any-Winter-4079 <50542132+Any-Winter-4079@users.noreply.github.com>
2024-04-08 15:19:33 +0200

    fix comment on iterations as they need to be the same for all methods -for now

--- computer_code/LLM/CoT_Dec_PAL_tester_v2.py
+++ computer_code/LLM/CoT_Dec_PAL_tester_v2.py
@@ -17,8 +17,8 @@ from utils.v2.utils import extract_and_format_value, load_exemplars, save_result
 # Dec: 0.553 (1337 seed) -> 0.572 (1337 seed) using more robust SymPy equations extraction.
 # PAL: 0.679 (1337 seed) -> 0.782 using 5-iter SC (Self-Consistency) [Best result].
 # You can choose one or more methods to test, each with one or more iterations (seeds) using SC.
-# For example, you can test the PAL method with 5 iterations (seeds), or the PAL method with 3 iterations (seeds)
-# and the CoT method with 2 iterations (seeds), for a total of 5 iterations (and thus results) to choose from.
+# For example, you can test the PAL method with 6 iterations (seeds), or the PAL method with 3 iterations (seeds)
+# and the CoT method with 3 iterations (seeds), for a total of 6 iterations (and thus results) to choose from.
 # Dec(with Simpy) is not tested with SC in my results because it is slower than the other methods (but it could be done).
 
 # /results: Contains the results of the tests (starting with v2).
