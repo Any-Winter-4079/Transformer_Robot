0bbc0350fbc061ef53a14b4174d28aa865af9d33
Any-Winter-4079 <50542132+Any-Winter-4079@users.noreply.github.com>
2024-06-26 16:15:08 +0200

    Create compare_with_heatmap.py

---
+++ computer_code/test_scripts/LLM/compare_with_heatmap.py
@@ -0,0 +1,64 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+
+#################
+# Description   #
+#################
+# This script visually compares the prediction results from two files.
+
+# seed_966 and seed_1337
+# Correct in both files: 589
+# Correct in file 1 only: 184
+# Correct in file 2 only: 193
+# Incorrect in both files: 353
+
+# seed_966 and seed_7625
+# Correct in both files: 586
+# Correct in file 1 only: 187
+# Correct in file 2 only: 204
+# Incorrect in both files: 342
+
+# seed_1337 and seed_7625
+# Correct in both files: 604
+# Correct in file 1 only: 178
+# Correct in file 2 only: 186
+# Incorrect in both files: 351
+
+# Which means we could theoretically improve the predictions up to >=0.7 accuracy where CoT with a single seed sits at ~0.58.
+# This is of course if we have a decider capable of choosing the best prediction from the two methods.
+
+# While comparing CoT and Declarative, I observed:
+# seed_1337 and seed_1337
+# Correct in both files: 528
+# Correct in file 1 only: 254
+# Correct in file 2 only: 227
+# Incorrect in both files: 310
+
+# Resulting in more diversity, but not by a large margin, which I found surprising.
+
+#################
+# venv          #
+#################
+# Remember to create a virtual environment, install the packages, and activate it.
+# In my case: source ./tensorflow-metal-test/bin/activate (from v2 folder)
+
+results_matrix = np.array([[528, 254], [227, 310]])
+
+methods = ["Correct", "Incorrect"]
+cmap = mcolors.LinearSegmentedColormap.from_list("custom", ["#FFB347", "#387761"], N=256)
+
+plt.figure(figsize=(8, 6))
+sns.heatmap(
+            results_matrix,
+            annot=True,
+            fmt="d",
+            cmap=cmap,
+            xticklabels=methods,
+            yticklabels=methods)
+
+plt.title('Mixtral-8x7b-instruct-Q5_0: CoT and Declarative overlap')
+plt.xlabel('Declarative')
+plt.ylabel('CoT')
+plt.show()
